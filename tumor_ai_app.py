af53799b981d3e934a610603a8bacdda8ce6fb73
oliverq11 <67991770+oliverq11@users.noreply.github.com>
2026-04-25 17:05:06 -0700

    Update tumor_ai_app.py

--- tumor_ai_app.py
+++ tumor_ai_app.py
@@ -8,7 +8,6 @@ import pandas as pd
 import qrcode
 from io import BytesIO
 import os
-st.write("Files in directory:", os.listdir("."))
 
 
 # ============================================================
